25ddc58c3da35902ac672bf0ed30c12aa78bf2ad
Christian Junge <jungecm@yahoo.com>
2015-01-24 23:46:34 -1000

    Added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,21 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This pair of functions works in conjuction to create an object with set and get methods
+##   for a matrix and its inverse.  
+## The inverse is cached so that it does not need to be recalculated, and the cached 
+##   inverse is cleared if the matrix is changed using the object's set methods.  
+
 
 ## This function creates a special "matrix" object that can cache its inverse.
 ##   (This description is copied from the assignment)
 
 makeCacheMatrix <- function(x = matrix()) {
      inv <- NULL
+     ## The starting value of the inverse is NULL
+     
      set <- function(y) {
           x <<- y
           inv <<- NULL
+          ## When the matrix is changed, the cached inverse is cleared
      }
+     
      get <- function() x
+     ## Returns the matrix itself
+     
      setInverse <- function(inverse) inv <<- inverse
+     ## Changes the cached inverse
+     
      getInverse <- function() inv
+     ## Returns the cached inverse
+     
      list(set = set, get = get,
           setInverse = setInverse,
           getInverse = getInverse)
+     ## Returns the "matrix object"
 }
 
 
@@ -26,12 +40,25 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
      inv <- x$getInverse()
+     ## Looks up the value of the cached inverse
+     
      if(!is.null(inv)) {
+          ## If there is a cached inverse, it is returned and the function exits
           message("getting cached data")
           return(inv)
      }
+     
+     ## If there is no cached inverse, the following executes:
+     
      data <- x$get()
+     ## Looks up the matrix itself
+     
      inv <- solve(data, ...)
+     ## Finds the inverse using the solve() function
+     
      x$setInverse(inv)
+     ## Sets the cached Inverse using the "matrix object"'s setInverse method
+     
      inv
+     ## Returns the newly computed inverse
 }
